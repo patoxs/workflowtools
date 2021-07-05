3030e3a9fabb5e529a599437076e51d3d0144be1
Patricio Gonzalez Leal <patonxs@gmail.com>
2021-07-05 15:51:54 -0400

    action basicas para deploy en k8s

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ function copyDirectory(o,d){
   }
 }
 
-function confK8S(c, n, r ){
+function confK8S(c, n){
   try {
     sequentialExecution(
-      "aws eks update-kubeconfig --name "+ c +" --region "+ r,
+      "aws eks update-kubeconfig --name "+ c +" --region "+ process.env.REGION,
       "kubectl config set-context --current --namespace="+n,
     );
     return true;
@@ -45,41 +45,78 @@ function confK8S(c, n, r ){
   }
 }
 
-
-async function uploadKanikoECR() {
+function uploadKanikoECR(branch, tag, app_name) {
   const identity = await sts.getCallerIdentity().promise();
   const ai = i.Account;
-  const t = hash();
-  var command = "kubectl run --rm kaniko-${APP_NAME}-"+ t +" --attach=true --image=gcr.io/kaniko-project/executor:latest --serviceaccount=${EKS_SAN} --restart=Never -- \
+  var sa = process.env.SERVICE_ACCOUNT;
+  console.log("repo" + app_name);
+  var command = "kubectl run --rm kaniko-"+ app_name +"-"+ tag +" --attach=true --image=gcr.io/kaniko-project/executor:latest --serviceaccount="+ sa +" --restart=Never -- \
         --verbosity=info \
-        --context=git://${{ secrets.TOKENKEY_GITHUB }}@github.com/${GITHUB_REPOSITORY} \
-        --destination="+ ai +".dkr.ecr.us-west-2.amazonaws.com/${IMAGE_REPOSITORY_NAME}:"+ t +" \
-        --destination="+ ai +".dkr.ecr.us-west-2.amazonaws.com/${IMAGE_REPOSITORY_NAME}:latest --git=branch=${GITHUB_BRANCH}"
-  return accountId;
+        --context=git://"+ process.env.TOKEN +"@github.com/"+ app_name +" \
+        --destination="+ ai +".dkr.ecr.us-west-2.amazonaws.com/"+ app_name +":"+ tag +" \
+        --destination="+ ai +".dkr.ecr.us-west-2.amazonaws.com/"+ app_name +":latest --git=branch="+ branch +" "
+  sequentialExecution( command );
 }
 
+function deployK8s(tag, app_name, n){
+  const identity = await sts.getCallerIdentity().promise();
+  const ai = i.Account;
+  sequentialExecution(
+    "kubectl set image --record deployment.apps/php php="+ ai +".dkr.ecr.us-west-2.amazonaws.com/"+ app_name +":"+ tag +" -n "+ n,
+    "kubectl rollout status deployment.apps/php -n "+ n,
+  );
+  return true;        
+}
+
+  // - name: default
+  //       uses: patoxs/workflowtools@main
+  //       with: 
+  //         action: 'default'
+  // - name: copy to artifacts
+  //       uses: patoxs/workflowtools@main
+  //       with: 
+  //         action: 'copyArtifacts' 
+  //         origin: 'path/directory/origin' 
+  //         destiny: 'path/directory/destiny'
+  // - name: configue k8s in runner
+  //       uses: patoxs/workflowtools@main
+  //       with: 
+  //         action: 'confK8S'
+  //         namespace: 'name_namespace'
+  // - name: Upload image to ECR
+  //       uses: patoxs/workflowtools@main
+  //       with: 
+  //         action: 'pushECR'
+  //         branch: 'branch_to_deploy'
+  //         app_name: 'app_name_or_ecr_name'
+  // - name: Deploy to K8s
+  //       uses: patoxs/workflowtools@main
+  //       with: 
+  //         action: 'deployK8s'
+  //         app_name: 'app_name_or_ecr_name'
+  //         namespace: 'name_namespace'
 
 try {
 
+  const tag = hash();
+  var gr = process.env.GITHUB_REPOSITORY;
+  var c = process.env.CLUSTER;
   const a = core.getInput('action', { required: true });
   const o = core.getInput('origin', { required: false });
   const d = core.getInput('destiny', { required: false });
-  const c = core.getInput('cluster', { required: false });
   const n = core.getInput('namespace', { required: false });
-  const r = core.getInput('region', { required: false });
+  const b = core.getInput('branch', { required: false });
+  const an = core.getInput('app_name', { required: false });
 
   console.log(`ACTION: ${a}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   if (a == "copyArtifacts") {copyDirectory(o,d)}
-  if (a == "confK8S") {confK8S(c,n,r)}
+  if (a == "confK8S") {confK8S(c,n)}
+  if (a == "pushECR") {uploadKanikoECR(b,tag,an)}
+  if (a == "deployK8s") {deployK8s(tag,an,n)}
   if (a == "default"){
-    console.log("github_repository ${GITHUB_REPOSITORY}");
-    console.log("github_actor ${GITHUB_ACTOR}");
-    console.log("github_event_name ${GITHUB_EVENT_NAME}");
-    console.log("github_ref ${GITHUB_REF}");
-    console.log("github_head_ref ${GITHUB_HEAD_REF}");
-    console.log("github_base_ref ${GITHUB_BASE_REF}");
+    console.log(${payload});
     console.log(process.env);
   }
   
