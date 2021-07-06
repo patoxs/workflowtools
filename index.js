c983eaac33344578900fb772c259d2435f25bfc4
Patricio Gonzalez Leal <patonxs@gmail.com>
2021-07-06 12:18:50 -0400

    refactorizacion push ecr kaniko

--- index.js
+++ index.js
@@ -35,7 +35,9 @@ function copyDirectory(o,d){
 }
 
 const ConfK8SPushEcr = async function(c, n, branch, tag, app_name, repo){
-  var kaniko = "kubectl run --rm kaniko-"+ app_name +"-"+ tag +" --attach=true --image=gcr.io/kaniko-project/executor:latest --serviceaccount="+ sa +" --restart=Never -- \
+  const identity = await sts.getCallerIdentity().promise();
+  const ai = identity.Account;
+  var kaniko = "kubectl run --rm kaniko-"+ app_name +"-"+ tag +" --attach=true --image=gcr.io/kaniko-project/executor:latest --serviceaccount="+ process.env.SERVICE_ACCOUNT +" --restart=Never -- \
         --verbosity=info \
         --context=git://"+ process.env.TOKEN +"@github.com/"+ app_name +" \
         --destination="+ ai +".dkr.ecr.us-west-2.amazonaws.com/"+ repo +":"+ tag +" \
